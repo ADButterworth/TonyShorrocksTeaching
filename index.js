3cb9104687df411d92a087fc68a1661b958e327f
Aaron Butterworth <aaron.david.butterworth@gmail.com>
2019-10-22 18:46:52 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+// sets the public folder
+app.use(express.static('public'));
+
+// set pug as the html templater
+app.set('view engine', 'pug');
+
+// Home Router
+app.get("/", function(req, res) {
+    res.redirect("/cv");
+});
+
+// Home Router
+app.get("/cv", function(req, res) {
+    res.render("home", {});
+});
+
+// Conducting Router
+app.get("/conducting", function(req, res) {
+    res.render("conducting", {});
+});
+
+// Coaching Router
+app.get("/coaching", function(req, res) {
+    res.render("coaching", {});
+});
+
+// Home Router
+app.get("/teaching", function(req, res) {
+    res.render("teaching", {});
+});
+
+// 404 Router
+// This is a generic router and must be last in the list to funtion correctly
+app.use(function(req, res) {
+    res.send("ERROR 404");
+});
+
+// Open the server on port 3000
+app.listen(3000);
